453f8b9e6dfb66d914fccc130ccd1f4d0d6b3bff
Haseeb Siddiqi <haseebs727@gmail.com>
2024-06-29 17:17:09 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {  Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import NoPage from './pages/NoPage'
+import Navbar from "./components/Navbar";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar/>
+    <div>
+    
+        <Routes>
+          <Route index element ={<Home/>} />
+          <Route path ="/home" element={<Home/> } />
+          <Route path ="/about" element={<About/> } />
+          <Route path ="/contact" element={<Contact/> } />
+          <Route path ="*" element={<NoPage/>} />
+        </Routes>
+      
     </div>
+    </>
   );
 }
 
